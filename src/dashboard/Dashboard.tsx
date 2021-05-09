fce75edbfa3acc86eae4b7b8343ecf9b64e64204
Antonis Anagnostou <anagnostou.antonis@gmail.com>
2021-05-09 20:17:37 +0100

    Adds mock service

--- src/dashboard/Dashboard.tsx
+++ src/dashboard/Dashboard.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import logo from "./logo.svg";
-import './Dashboard.css';
+import "./Dashboard.css";
+import getExperiments from "../mock-services/ExperimentService";
 
 export function Dashboard() {
+  let experiments = getExperiments();
+
   return (
     <div className="container block">
       <section className="hero is-primary">
@@ -18,42 +21,31 @@ export function Dashboard() {
       <br></br>
 
       <div className="block columns">
-        <div className="card column block">
-          <div className="card-content">
-            <div className="media">
-              <div className="media-left">
-                <figure className="image is-48x48">
-                  <img
-                    src="https://bulma.io/images/placeholders/96x96.png"
-                    alt="Placeholder image"
-                  />
-                </figure>
+        {experiments.map((exp) => (
+          <div className="card column block">
+            <div className="card-content">
+              <div className="media">
+                <div className="media-content">
+                  <p className="title is-4">{exp.name}</p>
+                </div>
               </div>
-              <div className="media-content">
-                <p className="title is-4">John Smith</p>
-                <p className="subtitle is-6">@johnsmith</p>
+
+              <div className="content">
+                {exp.description}
+                <br />
+                <time>{exp.dateCreated.toUTCString()}</time>
               </div>
             </div>
-
-            <div className="content">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
-              nec iaculis mauris. <a>@bulmaio</a>.<a href="#">#css</a>{" "}
-              <a href="#">#responsive</a>
-              <br />
-              <time dateTime="2016-1-1">11:09 PM - 1 Jan 2016</time>
-            </div>
           </div>
-        </div>
+        ))}
+
         <div className="card column block">
           <div className="card-content">
-            <a className="subtitle is-3 is-spaced" href="/wizard">Create new experiment</a>
+            <a className="subtitle is-3 is-spaced" href="/wizard">
+              Create new experiment
+            </a>
           </div>
         </div>
-
-        <div className="column"></div>
-        <div className="column"></div>
-        <div className="column"></div>
-
       </div>
     </div>
   );
---
+++ src/mock-services/ExperimentService.ts
@@ -0,0 +1,14 @@
+interface Experiment {
+    name: string;
+    dateCreated: Date;
+    description: string;
+}
+export default function getExperiments(): Experiment[] {
+    let experiments: Experiment[] = [];
+    experiments.push({ name: "Losing weight", description: "An experiment to lose weight", dateCreated: new Date() })
+    experiments.push({ name: "Getting a life", description: "An experiment to get a life", dateCreated: new Date() })
+    experiments.push({ name: "Getting a life 232", description: "An experiment to get a life", dateCreated: new Date() })
+
+
+    return experiments;
+}
