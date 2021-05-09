9932379fa8395df0a6c580f6f3436fb90875a48c
Antonis Anagnostou <anagnostou.antonis@gmail.com>
2021-05-09 18:31:31 +0100

    Changes dashboard

--- src/dashboard/Dashboard.tsx
+++ src/dashboard/Dashboard.tsx
@@ -1,18 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from "react";
+import logo from "./logo.svg";
 
 export function Dashboard() {
   return (
-    <section className="hero is-large is-info">
-    <div className="hero-body">
-      <p className="title">
-        Large hero
-      </p>
-      <p className="subtitle">
-        Large subtitle
-      </p>
+    <div className="container">
+      <div className="container">
+        <section className="hero is-primary">
+          <div className="hero-body">
+            <p className="title">Welcome, you crazy scientist!</p>
+            <p className="subtitle">
+              Here's an overview of all of your current experiments.
+            </p>
+          </div>
+        </section>
+      </div>
+
+      <div className="columns">
+        <div className="card column">
+          <div className="card-content">
+            <div className="media">
+              <div className="media-left">
+                <figure className="image is-48x48">
+                  <img
+                    src="https://bulma.io/images/placeholders/96x96.png"
+                    alt="Placeholder image"
+                  />
+                </figure>
+              </div>
+              <div className="media-content">
+                <p className="title is-4">John Smith</p>
+                <p className="subtitle is-6">@johnsmith</p>
+              </div>
+            </div>
+
+            <div className="content">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
+              nec iaculis mauris. <a>@bulmaio</a>.<a href="#">#css</a>{" "}
+              <a href="#">#responsive</a>
+              <br />
+              <time dateTime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+            </div>
+          </div>
+        </div>
+
+        <div className="column"></div>
+        <div className="column"></div>
+        <div className="column"></div>
+
+      </div>
     </div>
-  </section>
   );
 }
 
